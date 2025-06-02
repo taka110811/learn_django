462e73c040ba293cde791d92fa1d3b3ce706d29f
taka110811 <babysteps1108@gmail.com>
2025-06-02 14:02:52 +0900

    Modelを追加

--- myproject/crud/admin.py
+++ myproject/crud/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
+from django.contrib.admin.sites import AlreadyRegistered
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    search_fields = ('name',)
+
+try:
+    admin.site.register(Product, ProductAdmin)
+except AlreadyRegistered:
+    pass
