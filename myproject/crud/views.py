c6524fcc96a36d6de4602aafb9f80d5e704199b0
taka110811 <babysteps1108@gmail.com>
2025-06-02 14:07:39 +0900

    データ一覧の表示

--- myproject/crud/views.py
+++ myproject/crud/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Product
 
 class TopView(TemplateView):
     template_name = "top.html"
+
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 3
+    template_name = "list.html"
