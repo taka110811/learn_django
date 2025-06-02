44442108d84d8b50bfc580e96aaf2d077d99979c
taka110811 <babysteps1108@gmail.com>
2025-06-02 13:45:19 +0900

    hello world

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crud import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.TopView.as_view(), name="top"),
 ]
