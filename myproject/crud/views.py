34a086ff9c68b9475febf1fe95121d7429d12e76
taka110811 <142964603+taka110811@users.noreply.github.com>
2025-06-04 20:42:15 +0900

    Add product detail view with description

--- myproject/crud/views.py
+++ myproject/crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Product
 from django.urls import reverse_lazy
@@ -26,6 +26,9 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('list')
+
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    model = Product
 
 class LoginView(LoginView):
     form_class = AuthenticationForm
