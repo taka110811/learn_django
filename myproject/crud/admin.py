88d368023fc9a9684770be6d5e79e77d40ac16c8
taka110811 <babysteps1108@gmail.com>
2025-06-04 09:40:12 +0900

    データにリレーション（カテゴリー）を追加

--- myproject/crud/admin.py
+++ myproject/crud/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 from django.contrib.admin.sites import AlreadyRegistered
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price')
+    list_display = ('id', 'name', 'price', 'category')
+    search_fields = ('name',)
+    list_filter = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
     search_fields = ('name',)
 
 try:
     admin.site.register(Product, ProductAdmin)
+    admin.site.register(Category, CategoryAdmin)
 except AlreadyRegistered:
     pass
